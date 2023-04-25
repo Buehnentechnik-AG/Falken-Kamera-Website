ce4a25fb4c7f2c41d75f0bd7ed7075e8fbcfa82f
Lennart Bergmann <lennart.bergmann05@gmail.com>
2023-04-25 15:15:04 +0200

    Bug fix + Highlight Images
    sticky navbar fix for galery site

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="styles/default.css">
     <link rel="stylesheet" href="styles/navbar.css">
     <link rel="stylesheet" href="style.css">
-
 </head>
 
 <body>
@@ -23,11 +22,12 @@
         </nav>
         <hr>
     </div>
-    <div id="content-wrapper">
+
+    <div id="wrapper">
         <div id="content">
+
             <h1>Herzlich Willkommen!</h1>
             <img src="assets/divider.jpg" class="divider">
-
             <p>Im Rahmen der Projektwoche haben Schüler:innen eine Videokamera in einem Falken-Nistkasten installiert.
                 Der
                 Nistkasten befindet sich im Kirchturm der Grabeskirche <a href="https://www.trostraum.de/"><b>Trostraum
@@ -36,18 +36,20 @@
                 wurde dieses Projekt
                 geplant und umgesetzt. Finanziert wurde das Projekt von der Stadt Mönchengladbach – Quartiersmanagement
                 Rheydt.</p>
+
             <h2>Highlights</h2>
             <img src="assets/divider.jpg" class="divider">
             <div id="highlights">
-                <img class="zoomable" src="assets/blog/test.jpeg" width="50%" style="mind-width:50%;">
-                <img class="zoomable" src="assets/blog/test.jpeg" width="50%" style="mind-width:50%;">
-                <img class="zoomable" src="assets/blog/test.jpeg" width="50%" style="mind-width:50%;">
-                <img class="zoomable" src="assets/blog/test.jpeg" width="50%" style="mind-width:50%;">
-                <img class="zoomable" src="assets/blog/test.jpeg" width="50%" style="mind-width:50%;">
-                <img class="zoomable" src="assets/blog/test.jpeg" width="50%" style="mind-width:50%;">
+                <img class="zoomable" src="assets/blog/Highlight1.jpeg" width="50%" style="mind-width:50%;">
+                <img class="zoomable" src="assets/blog/Highlight2.jpeg" width="50%" style="mind-width:50%;">
+                <img class="zoomable" src="assets/blog/Highlight4.jpeg" width="50%" style="mind-width:50%;">
+                <img class="zoomable" src="assets/blog/Highlight5.jpeg" width="50%" style="mind-width:50%;">
+                <img class="zoomable" src="assets/blog/Highlight3.jpeg" width="50%" style="mind-width:50%;">
             </div>
+
         </div>
     </div>
+
     <script src="scripts/fullscreen.js"></script>
     <script src="scripts/navbar.js"></script>
 </body>
